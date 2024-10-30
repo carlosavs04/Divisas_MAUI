6cb66fe860b75f583b001d49ec618cf8c4f5abb5
Luis Angel Zapata <dev.luis.zapata@gmail.com>
2024-10-29 20:36:22 -0600

    feat: added ticket view and color label

--- Divisas/Views/Home.xaml.cs
+++ Divisas/Views/Home.xaml.cs
@@ -53,4 +53,9 @@ public partial class Home : ContentPage
 
         _viewModel.ConvertCurrencyCommand.Execute(null);
     }
+
+	private void GenerateTicket(object sender, EventArgs e)
+	{
+		   Navigation.PushModalAsync(new Ticket());
+	}
 }
---
+++ Divisas/Views/Ticket.xaml.cs
@@ -0,0 +1,9 @@
+namespace Divisas.Views;
+
+public partial class Ticket : ContentPage
+{
+	public Ticket()
+	{
+		InitializeComponent();
+	}
+}
